6832d6120e03550408bba13846a7815d9febb441
ahiemann <hiemann.andreas@gmail.com>
2020-11-12 15:33:57 +0100

    Adapted tests after merge to reflect different changes

--- src/test/scala/controllers/GameLogicSpec.scala
+++ src/test/scala/controllers/GameLogicSpec.scala
@@ -121,57 +121,117 @@ class GameLogicSpec extends AnyWordSpec with Matchers {
       val round10Result = gameLogic.setChip(Success(RoundModel(3, round9MatchField, player1)))
       val round10MatchField = round10Result.get.matchField
 
-      gameLogic.countDiagonal(6,0,0,"",0,0, round10MatchField.rows.map(_.map(_.name))) should be (4)
+      gameLogic.countDiagonal(6,0,0,"",0,0, round10MatchField.rows.map(_.map(_.name))) should be (3)
     }
 
     "return the boolean of successively tokens diagonal" in{
-      val move1 = gameLogic.setChip(0, initialField, player).get
-      val move2 = gameLogic.setChip(1, move1, player).get
-      val move3 = gameLogic.setChip(1, move2, player).get
-      val move4 = gameLogic.setChip(2, move3, player).get
-      val move5 = gameLogic.setChip(2, move4, player).get
-      val move6 = gameLogic.setChip(2, move5, player).get
-      val move7 = gameLogic.setChip(3, move6, new PlayerModel("Hans Peter", 'o')).get
-      val move8 = gameLogic.setChip(3, move7, player).get
-      val move9 = gameLogic.setChip(3, move8, player).get
-      val move10 = gameLogic.setChip(3, move9, player).get
-      gameLogic.checkIfSomeoneWon(move10, player) should be (Some(true))
+      val round1Result = gameLogic.setChip(initialRoundModel)
+      val round1MatchField = round1Result.get.matchField
+
+      val round2Result = gameLogic.setChip(Success(RoundModel(1, round1MatchField, player1)))
+      val round2MatchField = round2Result.get.matchField
+
+      val round3Result = gameLogic.setChip(Success(RoundModel(1, round2MatchField, player1)))
+      val round3MatchField = round3Result.get.matchField
+
+      val round4Result = gameLogic.setChip(Success(RoundModel(2, round3MatchField, player1)))
+      val round4MatchField = round4Result.get.matchField
+
+      val round5Result = gameLogic.setChip(Success(RoundModel(2, round4MatchField, player1)))
+      val round5MatchField = round5Result.get.matchField
+
+      val round6Result = gameLogic.setChip(Success(RoundModel(2, round5MatchField, player1)))
+      val round6MatchField = round6Result.get.matchField
+
+      val round7Result = gameLogic.setChip(Success(RoundModel(3, round6MatchField, PlayerModel("Hans Peter", 'o'))))
+      val round7MatchField = round7Result.get.matchField
+
+      val round8Result = gameLogic.setChip(Success(RoundModel(3, round7MatchField, player1)))
+      val round8MatchField = round8Result.get.matchField
+
+      val round9Result = gameLogic.setChip(Success(RoundModel(3, round8MatchField, player1)))
+      val round9MatchField = round9Result.get.matchField
+
+      val round10Result = gameLogic.setChip(Success(RoundModel(3, round9MatchField, player1)))
+      val round10MatchField = round10Result.get.matchField
+
+      gameLogic.checkIfSomeoneWon(round10MatchField, player1) should be (true)
     }
 
     "return true if player won with 4 tokens horizontal" in {
-      val move1 = gameLogic.setChip(0, initialField, player).get
-      val move2 = gameLogic.setChip(1, move1, player).get
-      val move3 = gameLogic.setChip(2, move2, player).get
-      val move4 = gameLogic.setChip(3, move3, player).get
-      gameLogic.checkIfSomeoneWon(move4, player) should be (Some(true))
+      val round1Result = gameLogic.setChip(initialRoundModel)
+      val round1MatchField = round1Result.get.matchField
+
+      val round2Result = gameLogic.setChip(Success(RoundModel(1, round1MatchField, player1)))
+      val round2MatchField = round2Result.get.matchField
+
+      val round3Result = gameLogic.setChip(Success(RoundModel(2, round2MatchField, player1)))
+      val round3MatchField = round3Result.get.matchField
+
+      val round4Result = gameLogic.setChip(Success(RoundModel(3, round3MatchField, player1)))
+      val round4MatchField = round4Result.get.matchField
+
+      gameLogic.checkIfSomeoneWon(round4MatchField, player1) should be (true)
     }
 
     "return true if player won with 4 tokens vertical" in {
-      val move1 = gameLogic.setChip(0, initialField, player).get
-      val move2 = gameLogic.setChip(0, move1, player).get
-      val move3 = gameLogic.setChip(0, move2, player).get
-      val move4 = gameLogic.setChip(0, move3, player).get
-      gameLogic.checkIfSomeoneWon(move4, player) should be (Some(true))
+      val round1Result = gameLogic.setChip(initialRoundModel)
+      val round1MatchField = round1Result.get.matchField
+
+      val round2Result = gameLogic.setChip(Success(RoundModel(0, round1MatchField, player1)))
+      val round2MatchField = round2Result.get.matchField
+
+      val round3Result = gameLogic.setChip(Success(RoundModel(0, round2MatchField, player1)))
+      val round3MatchField = round3Result.get.matchField
+
+      val round4Result = gameLogic.setChip(Success(RoundModel(0, round3MatchField, player1)))
+      val round4MatchField = round4Result.get.matchField
+
+      gameLogic.checkIfSomeoneWon(round4MatchField, player1) should be (true)
     }
 
     "return true if 4 tokens are vertical from position 0|0 -> 3|3"  in {
-      val move1 = gameLogic.setChip(0, initialField, player).get
-      val move2 = gameLogic.setChip(1, move1, player).get
-      val move3 = gameLogic.setChip(1, move2, player).get
-      val move4 = gameLogic.setChip(2, move3, player).get
-      val move5 = gameLogic.setChip(2, move4, player).get
-      val move6 = gameLogic.setChip(2, move5, player).get
-      val move7 = gameLogic.setChip(3, move6, new PlayerModel("Hans Peter", 'o')).get
-      val move8 = gameLogic.setChip(3, move7, player).get
-      val move9 = gameLogic.setChip(3, move8, player).get
-      val move10 = gameLogic.setChip(3, move9, player).get
-      gameLogic.countDiagonal(6,0,0,"",0,0, move10.rows.map(_.map(_.name))) should be (3)
+      val round1Result = gameLogic.setChip(initialRoundModel)
+      val round1MatchField = round1Result.get.matchField
+
+      val round2Result = gameLogic.setChip(Success(RoundModel(1, round1MatchField, player1)))
+      val round2MatchField = round2Result.get.matchField
+
+      val round3Result = gameLogic.setChip(Success(RoundModel(1, round2MatchField, player1)))
+      val round3MatchField = round3Result.get.matchField
+
+      val round4Result = gameLogic.setChip(Success(RoundModel(2, round3MatchField, player1)))
+      val round4MatchField = round4Result.get.matchField
+
+      val round5Result = gameLogic.setChip(Success(RoundModel(2, round4MatchField, player1)))
+      val round5MatchField = round5Result.get.matchField
+
+      val round6Result = gameLogic.setChip(Success(RoundModel(2, round5MatchField, player1)))
+      val round6MatchField = round6Result.get.matchField
+
+      val round7Result = gameLogic.setChip(Success(RoundModel(3, round6MatchField, PlayerModel("Hans Peter", 'o'))))
+      val round7MatchField = round7Result.get.matchField
+
+      val round8Result = gameLogic.setChip(Success(RoundModel(3, round7MatchField, player1)))
+      val round8MatchField = round8Result.get.matchField
+
+      val round9Result = gameLogic.setChip(Success(RoundModel(3, round8MatchField, player1)))
+      val round9MatchField = round9Result.get.matchField
+
+      val round10Result = gameLogic.setChip(Success(RoundModel(3, round9MatchField, player1)))
+      val round10MatchField = round10Result.get.matchField
+
+      gameLogic.countDiagonal(6,0,0,"",0,0, round10MatchField.rows.map(_.map(_.name))) should be (3)
     }
 
     "return the boolean of unsuccessively tokens diagonal" in{
-      val move1 = gameLogic.setChip(0, initialField, player).get
-      val move2 = gameLogic.setChip(1, move1, player).get
-      gameLogic.checkIfSomeoneWon(move2, player) should be (Some(false))
+      val round1Result = gameLogic.setChip(initialRoundModel)
+      val round1MatchField = round1Result.get.matchField
+
+      val round2Result = gameLogic.setChip(Success(RoundModel(1, round1MatchField, player1)))
+      val round2MatchField = round2Result.get.matchField
+
+      gameLogic.checkIfSomeoneWon(round2MatchField, player1) should be (false)
     }
   }
 }
