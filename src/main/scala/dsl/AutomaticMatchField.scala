107313d93430b10b3e9fde7a5f3e2b5672b52781
ahiemann <hiemann.andreas@gmail.com>
2020-11-14 15:22:52 +0100

    Renamed "play" to "playback" and "helper" to "play"

--- src/main/scala/dsl/AutomaticMatchField.scala
+++ src/main/scala/dsl/AutomaticMatchField.scala
@@ -1,7 +1,6 @@
 package dsl
 
 import controllers.GameLogic
-import dsl.GameColumn.GameColumn
 import model.{MatchfieldModel, PlayerModel, RoundModel}
 
 import scala.annotation.tailrec
@@ -12,20 +11,20 @@ object AutomaticMatchField {
 
 
   implicit class AutomaticMatchfield(matchfield: MatchfieldModel[PlayerModel]) {
-    def play(gameColumns: GameColumnPlayerMapping*): MatchfieldModel[PlayerModel] = {
+    def playback(gameColumns: GameColumnPlayerMapping*): MatchfieldModel[PlayerModel] = {
 
       @tailrec
-      def helper(matchfield: MatchfieldModel[PlayerModel], gameColumns: Seq[GameColumnPlayerMapping]):MatchfieldModel[PlayerModel] = {
+      def play(matchfield: MatchfieldModel[PlayerModel], gameColumns: Seq[GameColumnPlayerMapping]):MatchfieldModel[PlayerModel] = {
         if (gameColumns.isEmpty)
           matchfield
         else {
           val gameColumn = gameColumns.head
           val resultMatchfield = GameLogic.setChip(Try(RoundModel(gameColumn.column, matchfield, gameColumn.player)))
-          helper(resultMatchfield.get.matchField, gameColumns.tail)
+          play(resultMatchfield.get.matchField, gameColumns.tail)
         }
       }
       
-      helper(matchfield, gameColumns)
+      play(matchfield, gameColumns)
     }
   }
 
