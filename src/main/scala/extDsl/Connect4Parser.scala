c244a20de0d363114addb8097026c5d83f778142
pasi_ <pascal.ulmer@htwg-konstanz.de>
2020-11-20 16:38:36 +0100

    Externe dsl for player expanded

--- src/main/scala/extDsl/Connect4Parser.scala
+++ src/main/scala/extDsl/Connect4Parser.scala
@@ -7,22 +7,31 @@ import scala.util.parsing.combinator.RegexParsers
 class Connect4Parser extends RegexParsers {
 
 
+  def parseDsl(input: String): Either[String, PlayerModel] =
+    parseAll(player, input) match {
+      case Success(t,_) => Right(t)
+      case NoSuccess(msg,_)=>
+        Left(s"Failed parsing: $msg")
+    }
 
   // private def round: Parser[RoundModel]
 
   private def player: Parser[PlayerModel] =
-    "Player 1 has a name" ~ name ~
+    "Player" ~ number ~ "has a name" ~ name ~
     "and a symbol" ~ symbol ^^ {
-      case _ ~ n ~ _ ~ s =>
+      case _ ~ _ ~ _ ~ n ~ _ ~ s =>
         PlayerModel(n,s.toList(0))
     }
 
   private def name: Parser[String] = lineOfText
 
   //matches a line of characters until line break
-  private def lineOfText: Parser[String] = """[\v]+""".r  ^^ (_.trim)
+  private def lineOfText: Parser[String] = """[^\v]+""".r  ^^ (_.trim)
 
   //matches one of the two available tokens
-  private def symbol: Parser[String] = """[x|o]"""
+  private def symbol: Parser[String] = """[x|o]""".r
+
+  //matches one of the two available tokens
+  private def number: Parser[String] = """[1|2]""".r
 
 }
