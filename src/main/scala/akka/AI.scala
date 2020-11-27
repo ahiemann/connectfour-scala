0a7945bdbd47fde045371a357da70d9b36c80b04
pasi_ <pascal.ulmer@htwg-konstanz.de>
2020-11-27 18:26:46 +0100

    Test

--- src/main/scala/akka/AI.scala
+++ src/main/scala/akka/AI.scala
@@ -1,7 +1,21 @@
 package akka
 
+import akka.AI.Test
 import akka.actor._
 
-class AI extends AbstractActor{
+object AI extends App{
+  class Test extends Actor {
+    def receive = {
+      case s:String => println("Hello" + s)
+      case _      => println("Nope")
+    }
+  }
+  val system = ActorSystem("Root")
+  val actor = system.actorOf(Props[Test], "A")
 
+  actor ! "Sfds"
+}
+
+object Main extends App {
+  val a = new Test
 }
