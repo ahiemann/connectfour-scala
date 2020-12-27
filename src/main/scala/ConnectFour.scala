42a6fa869b3b699653d7d50c3f37392f27b9e627
ahiemann <hiemann.andreas@gmail.com>
2020-12-27 18:08:36 +0100

    Top level main method

--- src/main/scala/ConnectFour.scala
+++ src/main/scala/ConnectFour.scala
@@ -5,50 +5,49 @@ import scala.annotation.tailrec
 import scala.io.StdIn
 import scala.util.{Failure, Success, Try}
 
-object ConnectFour:
 
-  def main(args: Array[String]): Unit = {
+@main def connectFour(player1Name:String, player2Name:String): Unit = {
 
-    // Create players
-    val player1 = GameLogic.getInitialPlayerModel(args(0), 'x')
-    val player2 = GameLogic.getInitialPlayerModel(args(1), 'o')
+  // Create players
+  val player1 = GameLogic.getInitialPlayerModel(player1Name, 'x')
+  val player2 = GameLogic.getInitialPlayerModel(player2Name, 'o')
 
-    println(s"Symbol of player ${player1.name} is ${player1.sign}")
-    println(s"Symbol of player ${player2.name} is ${player2.sign}")
+  println(s"Symbol of player ${player1.name} is ${player1.sign}")
+  println(s"Symbol of player ${player2.name} is ${player2.sign}")
 
-    // get random index between 0 and 1 and choose player that starts accordingly
-    val startPlayer = if scala.util.Random.nextInt(2) == 0 then player1 else player2
+  // get random index between 0 and 1 and choose player that starts accordingly
+  val startPlayer = if scala.util.Random.nextInt(2) == 0 then player1 else player2
 
-    @tailrec
-    def playRound(currentPlayer:PlayerModel, player1:PlayerModel, player2:PlayerModel, matchfield:MatchfieldModel[PlayerModel]):String = {
-      println(GameLogic.getMatchfieldOutput(Vector(player1, player2), matchfield))
-      println(s"${currentPlayer.name}, in which column should the chip be placed? ")
+  @tailrec
+  def playRound(currentPlayer: PlayerModel, player1: PlayerModel, player2: PlayerModel, matchfield: MatchfieldModel[PlayerModel]): String = {
+    println(GameLogic.getMatchfieldOutput(Vector(player1, player2), matchfield))
+    println(s"${currentPlayer.name}, in which column should the chip be placed? ")
 
-      val roundData = Try(StdIn.readInt()) match {
+    val roundData = Try(StdIn.readInt()) match {
 
-        case Success(inputIndex) =>
-          val realIndex = inputIndex - 1
-          Success(RoundModel(realIndex, matchfield, currentPlayer))
+      case Success(inputIndex) =>
+        val realIndex = inputIndex - 1
+        Success(RoundModel(realIndex, matchfield, currentPlayer))
 
-        case Failure(_) => Failure(Exception("Wrong input. Please type the number of the column where you would like to insert your chip"))
-      }
-
-      GameLogic.playRound(roundData) match {
-        case Success(r : RoundResultGameOver) => s"${r.gameOverReason}\n${r.matchfield}"
-        case Success(r : RoundResultMoveOk) =>
-          println(GameLogic.getMatchfieldOutput(Vector(player1, player2), r.matchfield))
-          val nextPlayer = if currentPlayer == player1 then player2 else player1
-          playRound(nextPlayer, player1, player2, r.matchfield)
-        case Failure(f) =>
-          println(f.getMessage)
-          playRound(currentPlayer, player1, player2, matchfield)
-        case _ => throw Exception("Unknown RoundResult")
-      }
-
+      case Failure(_) => Failure(Exception("Wrong input. Please type the number of the column where you would like to insert your chip"))
     }
 
-    // Start the game
-    val gameOverMessage = playRound(startPlayer, player1, player2, GameLogic.getInitialMatchField())
-    println(gameOverMessage)
+    GameLogic.playRound(roundData) match {
+      case Success(r: RoundResultGameOver) => s"${r.gameOverReason}\n${r.matchfield}"
+      case Success(r: RoundResultMoveOk) =>
+        println(GameLogic.getMatchfieldOutput(Vector(player1, player2), r.matchfield))
+        val nextPlayer = if currentPlayer == player1 then player2 else player1
+        playRound(nextPlayer, player1, player2, r.matchfield)
+      case Failure(f) =>
+        println(f.getMessage)
+        playRound(currentPlayer, player1, player2, matchfield)
+      case _ => throw Exception("Unknown RoundResult")
+    }
 
   }
+
+  // Start the game
+  val gameOverMessage = playRound(startPlayer, player1, player2, GameLogic.getInitialMatchField())
+  println(gameOverMessage)
+
+}
