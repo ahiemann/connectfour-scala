6eb8e408cc616c63c9cf57ebaca4bae0586e9c6a
ahiemann <hiemann.andreas@gmail.com>
2021-01-13 15:59:48 +0100

    Added test for playRound function

--- src/main/scala/ConnectFour.scala
+++ src/main/scala/ConnectFour.scala
@@ -7,9 +7,6 @@ import scala.annotation.tailrec
 
 object ConnectFour {
   def main(args: Array[String]): Unit = {
-    //val startField = GameLogic.getInitialMatchField() // initiales Feld
-    //val player1 = PlayerModel(args(0), 'x')
-    // val player2 = PlayerModel(args(1), 'o')
     val view = new Tui()
     val controller = new GameController(view)
     val models = controller.startGame()
@@ -19,16 +16,18 @@ object ConnectFour {
 
     val startPlayer = if (scala.util.Random.nextInt(2) == 0) player1 else player2
 
-    @tailrec
-    def playRound(view:Tui, controller:GameController, matchfield:MatchfieldModel[PlayerModel], player1:PlayerModel, player2:PlayerModel, currentPlayer:PlayerModel):Boolean = {
-      controller.playRound(matchfield, player1, player2, currentPlayer) match {
-        case Left(roundSuccessful) =>
-          val nextPlayer = roundSuccessful._1
-          val newMatchfield = roundSuccessful._2
-          playRound(view, controller, newMatchfield, player1, player2, nextPlayer)
-        case Right(GameOverFlag) => true // Game Over
-      }
-    }
+
     playRound(view, controller, startField, player1, player2, startPlayer)
+  }
+
+  @tailrec
+  def playRound(view:Tui, controller:GameController, matchfield:MatchfieldModel[PlayerModel], player1:PlayerModel, player2:PlayerModel, currentPlayer:PlayerModel):Boolean = {
+    controller.playRound(matchfield, player1, player2, currentPlayer) match {
+      case Left(roundSuccessful) =>
+        val nextPlayer = roundSuccessful._1
+        val newMatchfield = roundSuccessful._2
+        playRound(view, controller, newMatchfield, player1, player2, nextPlayer)
+      case Right(GameOverFlag) => true // Game Over
+    }
   }
 }
---
+++ src/test/scala/MainSpec.scala
@@ -0,0 +1,35 @@
+import controller.GameController
+import model.{GameOverFlag, RealPlayer}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import util.GameLogic
+import view.Tui
+
+import java.io.ByteArrayInputStream
+
+class MainSpec extends AnyWordSpec with Matchers {
+  "The Connect4 programm" should {
+    val player1 = RealPlayer("Max", 'x')
+    val player2 = RealPlayer("Tom", 'o')
+    val matchfield = GameLogic.getInitialMatchField()
+    val view = new Tui()
+    val controller = new GameController(view)
+
+    "End if a player has won" in {
+      val stdinString =
+        """1
+          |2
+          |1
+          |2
+          |1
+          |2
+          |1
+          |""".stripMargin
+
+      val stdin = new ByteArrayInputStream(stdinString.getBytes)
+      Console.withIn(stdin) {
+        ConnectFour.playRound(view, controller, matchfield, player1, player2, player1) should be (true)
+      }
+    }
+  }
+}
